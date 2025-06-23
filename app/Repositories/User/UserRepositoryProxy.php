3be9b5c06e18e4c3c9181b25854fd7a59d45880d
Zelvan Abdi Wijaya <zelvan1701@gmail.com>
2025-06-23 10:37:50 +0700

    feat: add cache in proxy layer

--- app/Repositories/User/UserRepositoryProxy.php
+++ app/Repositories/User/UserRepositoryProxy.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\User;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class UserRepositoryProxy implements UserRepositoryInterface
@@ -20,7 +21,18 @@ class UserRepositoryProxy implements UserRepositoryInterface
 
     public function getUsers()
     {
-        Log::info("[proxy] Get Users via Facade");
-        return $this->user_repo->getUsers();
+        $key = 'users_all';
+        $exp = now()->addMinutes(10);
+
+        if (Cache::has($key)) {
+            Log::info("[proxy] Get Users via Facade from cache");
+            return Cache::get($key);
+        };
+
+        $users = $this->user_repo->getUsers();
+        Cache::put($key, $users, $exp);
+
+        Log::info("[proxy] Get Users via Facade from repository");
+        return $users;
     }
 }
